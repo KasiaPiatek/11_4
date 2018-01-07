6eb53f5f0d96f7a46b5c65d1779f964700922894
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-01-07 20:06:29 +0100

    add script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price - price;
+	this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+	console.log("This phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var GalaxyS6 = new Phone ("Sansung", 2000, "grey");
+var One = new Phone("OnePlus", 1000, "black");
+
+iPhone6S.printInfo();
+GalaxyS6.printInfo();
+One.printInfo();
+
